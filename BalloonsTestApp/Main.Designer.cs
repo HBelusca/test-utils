45bb9e6b45e71a42caa7e3d370d3a8c9f4b68635
gigaherz <gigaherz@gmail.com>
2018-01-23 01:15:55 +0100

    Added notification area balloons test app (.NET 2.0)

---
+++ BalloonsTestApp/Main.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace TooltipsTestApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyIcon2 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyIcon3 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Icon";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Remove Icon";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 146);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 33);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Add Balloon 1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::TooltipsTestApp.Properties.Resources._008_Reminder_48x48_72;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 50);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::TooltipsTestApp.Properties.Resources._1447_eraser_48x48;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox2.Location = new System.Drawing.Point(160, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(115, 50);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(160, 146);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(115, 33);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Add Balloon 1";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(160, 107);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(115, 33);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Remove Icon";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(160, 68);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(115, 33);
+            this.button8.TabIndex = 5;
+            this.button8.Text = "Add Icon";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::TooltipsTestApp.Properties.Resources.Generic_Device1;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox3.Location = new System.Drawing.Point(303, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(115, 50);
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(303, 146);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(115, 33);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "Add Balloon 1";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(303, 107);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(115, 33);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "Remove Icon";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(303, 68);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(115, 33);
+            this.button12.TabIndex = 10;
+            this.button12.Text = "Add Icon";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "This is the first text content";
+            this.notifyIcon1.BalloonTipTitle = "Notification Balloon 1";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Notification Balloon 1";
+            // 
+            // notifyIcon2
+            // 
+            this.notifyIcon2.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
+            this.notifyIcon2.BalloonTipText = "This is the text for the second balloon";
+            this.notifyIcon2.BalloonTipTitle = "Notification Balloon 2";
+            this.notifyIcon2.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon2.Icon")));
+            this.notifyIcon2.Text = "Notification Balloon 2";
+            // 
+            // notifyIcon3
+            // 
+            this.notifyIcon3.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Error;
+            this.notifyIcon3.BalloonTipText = "This is the third balloon";
+            this.notifyIcon3.BalloonTipTitle = "Notification Balloon 3";
+            this.notifyIcon3.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon3.Icon")));
+            this.notifyIcon3.Text = "Notification Balloon 3";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 185);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 33);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Add Balloon 2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(160, 185);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(115, 33);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Add Balloon 2";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(303, 185);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(115, 33);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "Add Balloon 2";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 227);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "BalloonTestApp";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.NotifyIcon notifyIcon2;
+        private System.Windows.Forms.NotifyIcon notifyIcon3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ BalloonsTestApp/Main.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TooltipsTestApp
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = true;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            notifyIcon2.Visible = true;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            notifyIcon3.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            notifyIcon2.Visible = false;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            notifyIcon3.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.ShowBalloonTip(5000);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            notifyIcon2.ShowBalloonTip(5000);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            notifyIcon3.ShowBalloonTip(5000);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.ShowBalloonTip(5000, "Alt Title balloon 1", "Alt Text balloon 1", ToolTipIcon.None);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            notifyIcon2.ShowBalloonTip(5000, "Alt Title balloon 2", "Alt Text balloon 2", ToolTipIcon.Error);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            notifyIcon3.ShowBalloonTip(5000, "Alt Title balloon 3", "Alt Text balloon 3", ToolTipIcon.Info);
+        }
+    }
+}
